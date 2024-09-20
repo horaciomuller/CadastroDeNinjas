fe8680b75e30c915887a86b0e05ea328fd57009a
Fiasco <contato@java10x.dev>
2024-09-20 10:54:34 +0100

    Cria uma entidade no banco de dados apartir do NinjaModel [Aula3]

--- src/main/java/dev/java10x/CadastroDeNinjas/NinjaController.java
+++ src/main/java/dev/java10x/CadastroDeNinjas/NinjaController.java
@@ -5,7 +5,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping
-public class Controller {
+public class NinjaController {
     @GetMapping("/boasvindas")
     public String boasVindas() {
         return "Essa é minha primeira mensagem nessa rota";
---
+++ src/main/java/dev/java10x/CadastroDeNinjas/NinjaModel.java
@@ -0,0 +1,49 @@
+package dev.java10x.CadastroDeNinjas;
+
+import jakarta.persistence.*;
+
+@Entity
+@Table(name = "tb_cadastro")
+public class NinjaModel {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String nome;
+    private String email;
+    private int idade;
+
+    public NinjaModel() {
+    }
+
+    public NinjaModel(String nome, String email, int idade) {
+        this.nome = nome;
+        this.email = email;
+        this.idade = idade;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public int getIdade() {
+        return idade;
+    }
+
+    public void setIdade(int idade) {
+        this.idade = idade;
+    }
+
+}
