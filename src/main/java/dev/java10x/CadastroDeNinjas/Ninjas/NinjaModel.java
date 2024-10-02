657f513843d522477ffcf33fb22a6301e369c1f7
Fiasco <contato@java10x.dev>
2024-10-02 12:34:33 +0100

    fix: Adiciona o @Collumn na minha classe

--- src/main/java/dev/java10x/CadastroDeNinjas/Ninjas/NinjaModel.java
+++ src/main/java/dev/java10x/CadastroDeNinjas/Ninjas/NinjaModel.java
@@ -14,13 +14,19 @@ public class NinjaModel {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column (name = "id")
     private long id;
 
+    @Column (name = "nome")
     private String nome;
 
     @Column(unique = true)
     private String email;
 
+    @Column (name = "img_url")
+    private String imgUrl;
+
+    @Column (name = "idade")
     private int idade;
 
     // @ManyToOne - Um ninja tem uma unica missao
