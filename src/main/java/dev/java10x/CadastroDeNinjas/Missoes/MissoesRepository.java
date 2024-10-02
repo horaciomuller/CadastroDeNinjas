aa4c53e533d38f92aa8ba2de654df075695de679
Fiasco <contato@java10x.dev>
2024-10-02 11:12:27 +0100

    feat: Cria o Missoes Repository

---
+++ src/main/java/dev/java10x/CadastroDeNinjas/Missoes/MissoesRepository.java
@@ -0,0 +1,5 @@
+package dev.java10x.CadastroDeNinjas.Missoes;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface MissoesRepository extends JpaRepository<MissoesModel, Long> {
+}
